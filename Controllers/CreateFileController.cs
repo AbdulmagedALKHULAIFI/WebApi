d81dc75030d881f799817a3c3faf16c7e2fa5c14
AbdulmagedALKHULAIFI <43535031+AbdulmagedALKHULAIFI@users.noreply.github.com>
2021-09-22 20:42:02 +0200

    first commit

---
+++ Controllers/CreateFileController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FileController : ControllerBase
+    {
+
+        public IActionResult Index()
+        {
+            return Content("Hello To Create File page"); ;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Content("Methode get");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Upload(IFormFile file)
+        {
+            return Content("Upload file "+ file.FileName); 
+        }
+    }
+}
